34d2f01d7da3c0c9a72ee8a057a059087c011e77
Valentin Vareskic <valentin.vareskic@gmail.com>
2025-10-03 09:17:07 +0200

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+
+</script>
+
+<div class="w-full flex min-h-screen">
+    <div class="w-full bg-[url('https://woop14abphufecql.public.blob.vercel-storage.com/sketches/68de2a44f1b3ed2d35b84586.jpg')] bg-gray-600 bg-cover md:bg-contain bg-no-repeat bg-center"></div>
+</div>
+<div class="grid grid-cols-2 lg:grid-cols-5">
+    <div class="bg-[url('https://woop14abphufecql.public.blob.vercel-storage.com/sketches/68de2a44f1b3ed2d35b8457f.jpg')] h-80 bg-cover"></div>
+    <div class="bg-[url('https://woop14abphufecql.public.blob.vercel-storage.com/sketches/68de2a44f1b3ed2d35b84580.jpg')] h-80 bg-cover"></div>
+    <div class="bg-[url('https://woop14abphufecql.public.blob.vercel-storage.com/sketches/68de2a44f1b3ed2d35b84581.jpg')] h-80 bg-cover"></div>
+    <div class="bg-[url('https://woop14abphufecql.public.blob.vercel-storage.com/sketches/68de2a44f1b3ed2d35b84583.jpg')] h-80 bg-cover"></div>
+    <div class="bg-[url('https://woop14abphufecql.public.blob.vercel-storage.com/sketches/68de2a44f1b3ed2d35b84582.jpg')] h-80 bg-cover"></div>
+    <div class="bg-[url('https://woop14abphufecql.public.blob.vercel-storage.com/sketches/68de2a44f1b3ed2d35b84584.jpg')] h-80 bg-cover"></div>
+    <div class="bg-[url('https://woop14abphufecql.public.blob.vercel-storage.com/sketches/68de2a44f1b3ed2d35b84585.jpg')] h-80 bg-cover"></div>
+    <div class="bg-[url('https://woop14abphufecql.public.blob.vercel-storage.com/sketches/68de2a44f1b3ed2d35b84587.jpg')] h-80 bg-cover"></div>
+    <div class="bg-[url('https://woop14abphufecql.public.blob.vercel-storage.com/sketches/68de2c9af1b3ed2d35b84588.jpg')] h-80 bg-cover"></div>
+</div>
